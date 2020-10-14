58ae69b17112c8ca1bef016fcdf6f942ae6573ec
Aleksejs Kalinins <alex.kalinins@mail.utoronto.ca>
2020-10-14 19:24:59 -0400

    Crash JVM

--- src/UnsafeBuster.java
+++ src/UnsafeBuster.java
@@ -3,14 +3,17 @@ import sun.misc.Unsafe;
 
 public class UnsafeBuster {
     public static void main(String[] args) {
-        long counter = 0;
-        long garbageSize = Integer.MAX_VALUE;
-        Unsafe unsafe = Garbage.getUnsafe();
+//        long counter = 0;
+//        long garbageSize = Integer.MAX_VALUE;
+//        Unsafe unsafe = Garbage.getUnsafe();
+//
+//        while (true) {
+//            unsafe.allocateMemory(garbageSize);
+//            System.out.printf("Garbage Created: %d%n", ++counter);
+//        }
 
-        while (true) {
-            unsafe.allocateMemory(garbageSize);
-            System.out.printf("Garbage Created: %d%n", ++counter);
-        }
+        Unsafe unsafe = Garbage.getUnsafe();
+        unsafe.getByte(0); // Notice: address 0 has never been allocated; will crash JVM.
 
     }
 }
